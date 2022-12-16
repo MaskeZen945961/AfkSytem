2dff00acae3569c1b75dc7b4fb3ab8eb219ae45c
MaskeZen <91011274+MaskeZen945961@users.noreply.github.com>
2022-12-16 17:57:52 -0500

    Update config.lua

--- AfkSystem/shared/config.lua
+++ AfkSystem/shared/config.lua
@@ -4,7 +4,7 @@ AfkMaskeZen = {
     entre = 1, -- le joueur gagne entre 1 et 500
     et = 500,  -- le joueur gagne entre 1 et 500
     nombrechance = 268, -- si le nombre est 268 alors le joueur gagne 20 000$
-    argentgagne = 20000, -- si le nombre est 268 alors le joueur gagne 20 000$
+    argentgagne = 20000, -- Argent gagner pour le nombre chance
     
 }
 
